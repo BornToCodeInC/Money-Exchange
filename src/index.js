6964d97e9e7f2b85164b301e3938539a92db40fe
Victoria <emilytoyacc@gmail.com>
2018-03-01 16:19:34 +0300

    money-exchange/feature: Implement makeExchange fanction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    let nomination = ["H","Q","D","N","P"];
+    let rate = [50,25,10,5,1];
+    let quantity = [];
+    let result = {};
+    let tmp;
+
+    if (currency <= 0) {
+        return {};
+    }
+    else if(currency >= 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else {
+        for (let i = 0; i < rate.length; i++) {
+            quantity.push(parseInt(currency / rate[i]));
+            currency = currency - quantity[i] * rate[i];
+        }
+    }
+    for (let i = 0; i < quantity.length; i++) {
+        if (quantity[i] > 0) {
+            tmp = nomination[i];
+            result[tmp] = quantity[i];
+        }
+    }
+    return result;
+};
